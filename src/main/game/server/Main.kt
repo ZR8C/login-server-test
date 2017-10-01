6007dc60c1506eab57081b07ab2a4e5a6848ea76
dave <david.goldfinch@datumsolutions.net>
2017-10-01 14:08:17 +0100

    try out BCrypt, enable kotlin-jackson

--- src/main/game/server/Main.kt
+++ src/main/game/server/Main.kt
@@ -1,16 +1,23 @@
 package game.server
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import game.server.rest.ServerApplication
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory
 import org.glassfish.jersey.server.ResourceConfig
 import javax.ws.rs.core.UriBuilder
+import javax.ws.rs.ext.ContextResolver
 
 fun main(args: Array<String>) {
     println("test")
 
     val uri = UriBuilder.fromUri("http://localhost/").port(9080).build()
 
-    val resource = ResourceConfig.forApplicationClass(ServerApplication::class.java)
+    val resource = ResourceConfig.forApplication(ServerApplication())
+            .register(ContextResolver<ObjectMapper> { ObjectMapper().registerModule(KotlinModule()) })
+
+//    val resource = ResourceConfig.forApplicationClass(ServerApplication::class.java)
+//            .register(ContextResolver<ObjectMapper> { ObjectMapper().registerModule(KotlinModule()) })
 
     val jettyServer = JettyHttpContainerFactory.createServer(uri, resource)
 
@@ -20,7 +27,5 @@ fun main(args: Array<String>) {
     } finally {
         jettyServer.destroy();
     }
-
-
 }
 
--- src/main/game/server/rest/Users.kt
+++ src/main/game/server/rest/Users.kt
@@ -1,11 +1,55 @@
 package game.server.rest
 
-import javax.ws.rs.GET
-import javax.ws.rs.Path
+import game.server.crypto.BCrypt
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+/**
+ * Todo: separate DB for passwords
+ * username | password
+ * send user details as body because URLs/parameters are usually output to logs
+ */
 
 @Path("/users/")
 class Users {
+    companion object {
+        val userPassword = mutableMapOf<String, String>();
+    }
 
-    @GET()
-    fun login() = "Logged in"
+    /**
+     * On successful login give back session token
+     */
+    @POST()
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    fun login(customer: Customer): Response? {
+        if(customer.userName.isBlank() || customer.password.isBlank())
+            return Response.status(400).entity("Username or password cannot be blank").build()
+
+        val hashedPW = userPassword.get(customer.userName)
+
+        if(BCrypt.checkpw(customer.password, hashedPW)) {
+            return Response.ok().entity("Login successful").build();
+        } else
+            return Response.status(400).entity("User/password not recognized").build();
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @POST()
+    @Path("register")
+    fun register(customer: Customer): Response? {
+
+        if(customer.userName.isBlank() || customer.password.isBlank())
+            return Response.status(400).entity("Username or password cannot be blank").build()
+
+        userPassword.put(customer.userName, BCrypt.hashpw(customer.password, BCrypt.gensalt()))
+
+        return Response.ok(customer).build();
+    }
+
 }
+
+data class Customer(val userName: String, val password: String)
